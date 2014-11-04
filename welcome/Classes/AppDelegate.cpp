39c4d9d646900a78f31042b27f1d38ddca4e7046
Iron Young <ironyoung@sjtu.edu.cn>
2014-11-05 00:17:06 +0800

    欢迎界面

---
+++ welcome/Classes/AppDelegate.cpp
@@ -0,0 +1,62 @@
+#include "AppDelegate.h"
+#include "WelcomeScene.h"
+#include "MeijiaMainScene.h"
+
+USING_NS_CC;
+
+AppDelegate::AppDelegate() {
+
+}
+
+AppDelegate::~AppDelegate() 
+{
+}
+
+bool AppDelegate::applicationDidFinishLaunching() {
+    // initialize director
+    auto director = Director::getInstance();
+    auto glview = director->getOpenGLView();
+    if(!glview) {
+        //glview = GLView::create("My Game");
+
+		// 开发人员的观察窗口大小
+		glview = GLView::createWithRect("My Game", Rect(0, 0, 768, 576));
+        director->setOpenGLView(glview);
+    }
+
+	// 适配机器的屏幕大小
+	glview->setDesignResolutionSize(1024, 768, ResolutionPolicy::EXACT_FIT);
+
+    // turn on display FPS
+    director->setDisplayStats(true);
+
+    // set FPS. the default value is 1.0/60 if you don't call this
+    director->setAnimationInterval(1.0 / 60);
+
+    // create a scene. it's an autorelease object
+	auto scene = Welcome::createScene();
+	// auto meijiaMain = MeijiaMain::createScene();
+
+    // run
+    director->runWithScene(scene);
+	// director->replaceScene(TransitionFade::create(0.5f, meijiaMain));
+	// director->replaceScene(meijiaMain);
+
+    return true;
+}
+
+// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
+void AppDelegate::applicationDidEnterBackground() {
+    Director::getInstance()->stopAnimation();
+
+    // if you use SimpleAudioEngine, it must be pause
+    // SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+}
+
+// this function will be called when the app is active again
+void AppDelegate::applicationWillEnterForeground() {
+    Director::getInstance()->startAnimation();
+
+    // if you use SimpleAudioEngine, it must resume here
+    // SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+}
---
+++ welcome/Classes/WelcomeScene.cpp
@@ -0,0 +1,112 @@
+#include "WelcomeScene.h"
+#include "MeijiaMainScene.h"
+
+USING_NS_CC;
+
+Scene* Welcome::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = Welcome::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool Welcome::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
+
+    // add a "close" icon to exit the progress. it's an autorelease object
+    auto closeItem = MenuItemImage::create(
+                                           "CloseNormal.png",
+                                           "CloseSelected.png",
+                                           CC_CALLBACK_1(Welcome::menuCloseCallback, this));
+    
+	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
+                                origin.y + closeItem->getContentSize().height/2));
+
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(closeItem, NULL);
+    menu->setPosition(Vec2::ZERO);
+    this->addChild(menu, 1);
+
+    /////////////////////////////
+    // 3. add your codes below...
+
+    // add a label shows "Hello World"
+    // create and initialize a label
+    
+    auto label = LabelTTF::create("meijia", "Arial", 80);
+    
+    // position the label on the center of the screen
+    label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                            origin.y + visibleSize.height/4*3));
+
+    // add the label as a child to this layer
+    this->addChild(label, 1);
+
+
+    // add "HelloWorld" splash screen"
+    auto sprite = Sprite::create("WelcomeBg.jpg");
+	// sprite->addChild(label, 1);
+
+    // position the sprite on the center of the screen
+    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+
+    // add the sprite as a child to this layer
+    this->addChild(sprite, 0);
+
+	//auto listener = EventListenerTouchOneByOne::create();
+	//listener->onTouchBegan = CC_CALLBACK_2(Welcome::onTouchBegan, this);
+	//_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+	scheduleOnce(schedule_selector(Welcome::transToMeijia), 0.1f);
+
+    return true;
+}
+
+
+void Welcome::menuCloseCallback(Ref* pSender)
+{
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    return;
+#endif
+
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
+
+//bool Welcome::onTouchBegan(cocos2d::CCTouch* pTouch, cocos2d::CCEvent* pEvent)
+//{
+//	auto meijiaScene = MeijiaMain::createScene();
+//	Director::getInstance()->replaceScene(TransitionRotoZoom::create(0.2f, meijiaScene));
+//	return true;
+//}
+
+void Welcome::transToMeijia(float dt)
+{
+	auto meijiaScene = MeijiaMain::createScene();
+	Director::getInstance()->replaceScene(TransitionFade::create(2.f, meijiaScene));
+}
