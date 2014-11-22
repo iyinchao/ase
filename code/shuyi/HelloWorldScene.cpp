7673106c415fac56c803ddd5f868ac375480b8bc
ase_shuyi <ase_shuyi@123.com>
2014-11-23 00:01:06 +0800

    评估报告，文件夹调整

--- code/shuyi/HelloWorldScene.cpp
+++ code/shuyi/HelloWorldScene.cpp
@@ -145,6 +145,93 @@ bool HelloWorld::init()
     return true;
 }
 
+void HelloWorld::sliderR(Ref *sender,SliderEventType type){
+	auto slider = (Slider*)sender;
+	if(curObj){
+        if(oldObj)
+            free(oldObj);
+        
+        oldObj = (objInfo*)malloc(sizeof(objInfo));
+        memcpy(oldObj, curObj, sizeof(objInfo));
+        
+		curObj->rgb[0] = slider->getPercent()/100.0;
+		state->setUniformVec4(COLOR,Vec4(curObj->rgb[0],curObj->rgb[1],curObj->rgb[2],1.0));
+		stateColor->setUniformVec3("rgb",Vec3(curObj->rgb[0],curObj->rgb[1],curObj->rgb[2]));
+        stateG->setUniformVec3("rgb",Vec3(curObj->rgb[0],curObj->rgb[1],curObj->rgb[2]));
+        stateB->setUniformVec3("rgb",Vec3(curObj->rgb[0],curObj->rgb[1],curObj->rgb[2]));
+		return;
+	}
+	else{
+		if(curLight){
+            if(oldLight)
+                free(oldLight);
+            
+            oldLight = (lightInfo*)malloc(sizeof(lightInfo));
+            memcpy(oldLight, curLight, sizeof(lightInfo));
+            
+			curLight->rgb[0] = slider->getPercent()/100.0;
+			state->setUniformVec4(COLOR,Vec4(curLight->rgb[0],curLight->rgb[1],curLight->rgb[2],curLight->intensity));
+			stateColor->setUniformVec3("rgb",Vec3(curLight->rgb[0],curLight->rgb[1],curLight->rgb[2]));
+            stateG->setUniformVec3("rgb",Vec3(curLight->rgb[0],curLight->rgb[1],curLight->rgb[2]));
+            stateB->setUniformVec3("rgb",Vec3(curLight->rgb[0],curLight->rgb[1],curLight->rgb[2]));
+		}
+	}
+}
+
+void HelloWorld::sliderG(Ref *sender,SliderEventType type){
+	auto slider = (Slider*)sender;
+	if(curObj){
+		curObj->rgb[1] = slider->getPercent()/100.0;
+		state->setUniformVec4(COLOR,Vec4(curObj->rgb[0],curObj->rgb[1],curObj->rgb[2],1.0));
+		stateColor->setUniformVec3("rgb",Vec3(curObj->rgb[0],curObj->rgb[1],curObj->rgb[2]));
+        stateR->setUniformVec3("rgb",Vec3(curObj->rgb[0],curObj->rgb[1],curObj->rgb[2]));
+        stateB->setUniformVec3("rgb",Vec3(curObj->rgb[0],curObj->rgb[1],curObj->rgb[2]));
+		return;
+	}
+	else{
+		if(curLight){
+			curLight->rgb[1] = slider->getPercent()/100.0;
+			state->setUniformVec4(COLOR,Vec4(curLight->rgb[0],curLight->rgb[1],curLight->rgb[2],curLight->intensity));
+			stateColor->setUniformVec3("rgb",Vec3(curLight->rgb[0],curLight->rgb[1],curLight->rgb[2]));
+            stateR->setUniformVec3("rgb",Vec3(curLight->rgb[0],curLight->rgb[1],curLight->rgb[2]));
+            stateB->setUniformVec3("rgb",Vec3(curLight->rgb[0],curLight->rgb[1],curLight->rgb[2]));
+		}
+	}
+}
+
+void HelloWorld::sliderB(Ref *sender,SliderEventType type){
+	auto slider = (Slider*)sender;
+	if(curObj){
+		curObj->rgb[2] = slider->getPercent()/100.0;
+		state->setUniformVec4(COLOR,Vec4(curObj->rgb[0],curObj->rgb[1],curObj->rgb[2],1.0));
+		stateColor->setUniformVec3("rgb",Vec3(curObj->rgb[0],curObj->rgb[1],curObj->rgb[2]));
+        stateR->setUniformVec3("rgb",Vec3(curObj->rgb[0],curObj->rgb[1],curObj->rgb[2]));
+        stateG->setUniformVec3("rgb",Vec3(curObj->rgb[0],curObj->rgb[1],curObj->rgb[2]));
+		return;
+	}
+	else{
+		if(curLight){
+			curLight->rgb[2] = slider->getPercent()/100.0;
+			state->setUniformVec4(COLOR,Vec4(curLight->rgb[0],curLight->rgb[1],curLight->rgb[2],curLight->intensity));
+			stateColor->setUniformVec3("rgb",Vec3(curLight->rgb[0],curLight->rgb[1],curLight->rgb[2]));
+            stateR->setUniformVec3("rgb",Vec3(curLight->rgb[0],curLight->rgb[1],curLight->rgb[2]));
+            stateG->setUniformVec3("rgb",Vec3(curLight->rgb[0],curLight->rgb[1],curLight->rgb[2]));
+		}
+	}
+}
+
+void HelloWorld::sliderL(Ref *sender,SliderEventType type){
+	auto slider = (Slider*)sender;
+	if(curObj){
+		return;
+	}
+	else{
+		if(curLight){
+			curLight->intensity = slider->getPercent()/100.0;
+			state->setUniformVec4(COLOR,Vec4(curLight->rgb[0],curLight->rgb[1],curLight->rgb[2],curLight->intensity));
+		}
+	}
+}
 
 //read contentfile and get objects and lights, their names are stored in objFiles and lightFiles
 bool HelloWorld::getTexFileName(const char* contentfile){
@@ -319,11 +406,200 @@ Texture2D *HelloWorld::loadUserTex(const char* filename){
 	return Director::getInstance()->getTextureCache()->addImage(fullpath);
 }
 
+bool HelloWorld::initTexture(){
+
+	//gen RTT texture
+
+	OPInfo *temp = NULL;
+
+	//init object
+	if(objCount > 0){
+		for(int i=0;i<objCount;i++){
+			temp = new OPInfo;
+			temp->type = ADDOBJ;
+			temp->arg1 = i;
+			temp->next = curOP;
+			curOP = temp;
+
+			temp = new OPInfo;
+			temp->type = REMOVEOBJ;
+			temp->arg1 = i;
+			temp->next = curOP;
+			curOP = temp;
+		}
+
+		temp = new OPInfo;
+		temp->type = INIT;
+		temp->next = curOP;
+		curOP = temp;
+	}
+
+	//init light
+	if(lightCount > 0){
+		for(int i=0;i<lightCount;i++){
+			temp = new OPInfo;
+			temp->type = ADDLIGHT;
+			temp->arg1 = i;
+			temp->next = curOP;
+			curOP = temp;
+		}
+	}
+	else{
+		temp = new OPInfo;
+		temp->type = ADDLIGHT;
+		temp->arg1 = 0;
+		temp->next = curOP;
+		curOP = temp;
+	}
+
+	temp = new OPInfo;
+	temp->type = CLEAR;
+	temp->next = curOP;
+	curOP = temp;
+
+	//objects[0]->tex = Director::getInstance()->getTextureCache()->addImage("s2.png");
+
+	return true;
+}
+
+//add light l to lightTex
+bool HelloWorld::addLight(lightInfo *l){
+	if(!l)
+		return false;
+    
+	if(!l->bace){
+		char *temp = getFilePath(l->lightFile);
+		l->bace = loadTex(temp);
+		free(temp);
+		if(l->bace == NULL)
+			return false;
+	}
+    
+	state->setUniformInt(STATE,5);
+	state->setUniformTexture(LIGHT,l->bace);
+	state->setUniformTexture(BACE,light);
+	state->setUniformVec4(COLOR,Vec4(l->rgb[0],l->rgb[1],l->rgb[2],l->intensity));
+    
+	//Sprite *p = (Sprite*)this->getChildByTag(picTag);
+	//myRTT->beginWithClear(0,0,0,0);
+	//p->visit();
+	//myRTT->end();
+    
+	return true;
+}
+
+//remove light l from lightTex
+bool HelloWorld::removeLight(lightInfo *l){
+	if(!l)
+		return false;
+    
+	if(!l->bace){
+		char *temp = getFilePath(l->lightFile);
+		l->bace = loadTex(temp);
+		free(temp);
+		if(l->bace == NULL)
+			return false;
+	}
+    
+	state->setUniformInt(STATE,6);
+	state->setUniformTexture(LIGHT,l->bace);
+	state->setUniformTexture(BACE,light);
+	state->setUniformVec4(COLOR,Vec4(l->rgb[0],l->rgb[1],l->rgb[2],l->intensity));
+	
+	Sprite *p = (Sprite*)this->getChildByTag(picTag);
+	myRTT->beginWithClear(0,0,0,0);
+	p->visit();
+	myRTT->end();
+    
+	return true;
+}
+
+//for modifying light intensity.
+//remove target light from lightmap and change shader state
+bool HelloWorld::setCurLight(int lightNum){
+	int num = -1;
+	int num2 = -1;
+
+	if(curObj){
+		for(int i=0;i<objCount;i++){
+			if(objects[i] == curObj){
+				num2 = i;
+				break;
+			}
+		}
+
+		curObj = nullptr;
+	}
+
+	if(curLight){
+		for(int i=0;i<lightCount;i++)
+			if(lights[i] == curLight){
+				num = i;
+				break;
+			}
+	}
+	curLight = lights[lightNum];
+
+	OPInfo *temp = new OPInfo;
+
+	temp->next = curOP;
+	temp->type = NORMALLIGHT;
+	temp->arg1 = lightNum;
+	curOP = temp;
+
+	temp = new OPInfo;
+	temp->next = curOP;
+	temp->type = REMOVELIGHT;
+	temp->arg1 = lightNum;
+	curOP = temp;
+
+	if(num != -1){
+		temp = new OPInfo;
+		temp->next = curOP;
+		temp->type = ADDLIGHT;
+		temp->arg1 = num;
+		curOP = temp;
+	}
+
+	if(num2 != -1){
+		temp = new OPInfo;
+		temp->next = curOP;
+		temp->type = ADDOBJ;
+		temp->arg1 = num2;
+		curOP = temp;
+	}
+
+	return true;
+}
+
 void HelloWorld::useShader(Sprite *sprite){
+	if(sprite){
+		GLProgram * p = new GLProgram();
+		p->initWithFilenames("myShader.vsh", "myShader.fsh");
+		p->bindAttribLocation(GLProgram::ATTRIBUTE_NAME_POSITION, GLProgram::VERTEX_ATTRIB_POSITION);
+		p->bindAttribLocation(GLProgram::ATTRIBUTE_NAME_COLOR, GLProgram::VERTEX_ATTRIB_COLOR);
+		p->bindAttribLocation(GLProgram::ATTRIBUTE_NAME_TEX_COORD, GLProgram::VERTEX_ATTRIB_TEX_COORDS);
+
+		p->link();
+		p->updateUniforms();
+		sprite->setShaderProgram(p);
+	} 
 }
 
 void HelloWorld::userShaderRGB(Sprite *sprite){
+	if(sprite){
+		GLProgram * p = new GLProgram();
+		p->initWithFilenames("rgbShader.vsh", "rgbShader.fsh");
+		p->bindAttribLocation(GLProgram::ATTRIBUTE_NAME_POSITION, GLProgram::VERTEX_ATTRIB_POSITION);
+		p->bindAttribLocation(GLProgram::ATTRIBUTE_NAME_COLOR, GLProgram::VERTEX_ATTRIB_COLOR);
+		p->bindAttribLocation(GLProgram::ATTRIBUTE_NAME_TEX_COORD, GLProgram::VERTEX_ATTRIB_TEX_COORDS);
+
+		p->link();
+		p->updateUniforms();
+		sprite->setShaderProgram(p);
+	} 
 }
+
 
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
@@ -412,4 +688,101 @@ void HelloWorld::bFullScreen(Object *sender,TouchEventType eventType){
 	if(eventType != TouchEventType::TOUCH_EVENT_ENDED)
 		return;
 
+}
+
+void HelloWorld::update(float dt){
+	this->unscheduleUpdate();
+
+	if(!curOP){
+        this->unscheduleUpdate();
+		return;
+	}
+
+	OPInfo *old;
+	Image *img;
+	Sprite *p = (Sprite*)this->getChildByTag(picTag);
+	switch(curOP->type){
+        case INIT:{
+            //set objALL to bace
+            myRTT3->beginWithClear(0,0,0,0);
+			p->visit();
+            //myRTT->getSprite()->visit();
+            myRTT3->end();
+			img = myRTT3->newImage();
+			img->saveToFile("E:\\ppp\\init_bace.png");
+			bace->initWithImage(img);
+
+            myRTT2->beginWithClear(0,0,0,0);
+            p->visit();
+            //myRTT->getSprite()->visit();
+            myRTT2->end();
+			img = myRTT2->newImage();
+			img->saveToFile("E:\\ppp\\init_objall.png");
+			objALL->initWithImage(img);
+            
+            old = curOP;
+            curOP = curOP->next;
+            delete old;
+            
+            break;}
+        case ADDLIGHT:
+            //add one light
+            addLight(lights[curOP->arg1]);
+
+            myRTT1->begin();
+            p->visit();
+            //myRTT->getSprite()->visit();
+            myRTT1->end();
+			img = myRTT1->newImage();
+			img->saveToFile("E:\\ppp\\addlight_light.png");
+			light->initWithImage(img);
+            
+            old = curOP;
+            curOP = curOP->next;
+            delete old;
+            
+            break;
+        case REMOVELIGHT:
+            //remove one light
+            removeLight(lights[curOP->arg1]);
+
+            myRTT1->begin();
+            p->visit();
+            //myRTT->getSprite()->visit();
+            myRTT1->end();
+			light->initWithImage(myRTT1->newImage());
+            
+            old = curOP;
+            curOP = curOP->next;
+            delete old;
+            
+            break;
+        case NORMALLIGHT:
+            //now is modifying a light
+            
+            state->setUniformInt(STATE,2);
+            state->setUniformTexture(BACE,light);
+            state->setUniformTexture(LIGHT,curLight->bace);
+            state->setUniformTexture(ORIGIN,bace);
+            state->setUniformTexture(OBJBACE,objALL);
+            state->setUniformVec4(COLOR,Vec4(curLight->rgb[0],curLight->rgb[1],curLight->rgb[2],curLight->intensity));
+            
+            old = curOP;
+            curOP = curOP->next;
+            delete old;
+            
+            //loadSprite->setVisible(false);
+            
+            break;
+        case CLEAR:
+            myRTT1->beginWithClear(0,0,0,0);
+            myRTT1->end();
+            
+            old = curOP;
+            curOP = curOP->next;
+            delete old;
+            
+            break;
+	}
+    
 }
