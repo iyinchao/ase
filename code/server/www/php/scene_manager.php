5a04aa1da430499c1e9c2f62df13a5a4fae7284f
Charles Yin <yincharles@163.com>
2014-12-06 00:16:15 +0800

    整合：scene_manager中的场景浏览接口；修改：接口规范文档（获取缩略图）

--- code/server/www/php/scene_manager.php
+++ code/server/www/php/scene_manager.php
@@ -63,6 +63,7 @@ class SceneManager{
         }
         //
     }
+
     public static function client_browse($data){
         //test if s_id data is set
         if(!isset($data->page_now) || !isset($data->scene_per_page)){
@@ -74,39 +75,59 @@ class SceneManager{
         }catch (Exception $e){
             exit('{"status":"ERROR_DB_CONN","error_message:"'.$e->getMessage().'"}');
         }
-        //
+        //inspect each data field & pre-process user input data
+        //'must have' field
+        $scene_per_page = mysqli_real_escape_string($db, $data->scene_per_page);
+        $page_now = mysqli_real_escape_string($db, $data->page_now);
+
+        $query = "select * from scene where 1=1 ";
+
+        if(isset($data->tag)){
+            $tags = (array)$data->tag;
+            if(sizeof($tags) != 0){
+                for($i = 0; $i < sizeof($tags); $i++){
+                    $tag = mysqli_real_escape_string($db,$tags[$i]);
+                    $query = $query."and Exists(select * from tag_scene where tag_scene.s_id = scene.s_id and tag_scene.tag = $tag)";
+                }
+            }
+        }
         if(isset($data->search)){
 
         }
         if(isset($data->sort_by)){
 
         }
-
-        if(isset($data->tag)){
-            $tags = (array)$data->tag;
-            //echo sizeof($tags);
-            if(sizeof($tags) != 0){
-
-                for($i = 0; $i < sizeof($tags); $i++){
-                    $tags[$i] = mysqli_real_escape_string($db,$tags[$i]);
+        $now_page = $page_now * $scene_per_page; //Notice: page starts from 0;
+        $query_count = "SELECT COUNT(s_id) AS num FROM ($query)temp";
+        $query = $query." limit $scene_per_page offset $now_page";
+        //echo $query;
+        $result = $db->query($query);
+        $scene = array();
+        if(($n = $result->num_rows) > 0){
+            for($i = 0; $i < $n; $i++){
+                $scene_obj = (object)array();
+                $row = $result->fetch_assoc();
+                foreach($row as $name => $value){
+                    $scene_obj->$name = $value;
                 }
-                $_qs_tags = join(',',$tags);
+                /*if(file_exists(Conf::DIR_DESIGN_FILE.$row['s_id'].'/thumb.png')){
+                    //$scene_obj->thumb = base64_encode(file_get_contents(Conf::DIR_DESIGN_FILE.$row['s_id'].'/thumb.png'));
+                }else{
+                    //$scene_obj->thumb = '';
+                }*/
+                $scene[$i] = json_encode($scene_obj);
             }
         }
-        $page_now = mysqli_real_escape_string($db, $data->page_now);
+        $result = $db->query($query_count);
+        $row = $result->fetch_assoc();
+        $page_all = ceil(((int)$row['num'])/(int)$scene_per_page);
 
-        if(isset($_qs_tags)){
-            $qs_tags = "SELECT s_id FROM bhouse.tag_scene WHERE tag IN ($_qs_tags)";
-            $result = $db->query($qs_tags);
-            if($row =$result->fetch_assoc()){
-                echo $row['s_id'];
-            }
-        }
-
-        $qs_count = "SELECT COUNT(s_id) FROM bhouse.scene";
-        $qs_content = "";
-
-        echo 'ok';
+        $response = (object)array();
+        $response->status = 'OK';
+        $response->scene = $scene;
+        $response->page_now = $page_now;
+        $response->page_all = $page_all;
+        echo json_encode($response);
 
     }
 }
