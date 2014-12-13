427b413a814b497ce05b390db5d62c2c1b37391f
Charles Yin <yincharles@163.com>
2014-12-13 20:41:12 +0800

    开发：后台管理系统，初始架构

---
+++ code/server/www/admin/js/scene.js
@@ -0,0 +1,52 @@
+/**
+ * Created by 尹超 on 2014/12/13.
+ */
+
+(function(){
+    var SCENE_PER_PAGE = 8;
+    function load_scene(page, tags, search){
+        var requ = {};
+        requ.scene_per_page = SCENE_PER_PAGE;
+        requ.tags = tags;
+        requ.page_now = page;
+        var form = new FormData();
+        form.append('op', 'client_browse');
+        form.append('data', JSON.stringify(requ));
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', '../php/scene_api.php', true);
+        xhr.addEventListener('readystatechange', function(e){
+            if (this.readyState == 4) {
+                if ((this.status >= 200 && this.status < 300) || this.status == 304) {
+                    var resp = JSON.parse(this.responseText);
+                    refresh_scene_view(resp);
+                    //console.log(resp);
+                }
+            }
+        });
+        xhr.send(form);
+    }
+
+    function refresh_scene_view(json){
+        $('#scene-view').empty();
+        json.scene.forEach(function (element, index, array) {
+            var cell_info = JSON.parse(element);
+            //console.log(element);
+            var cell = document.createElement('div');
+            cell.classList.add('mj-scene-block');
+            $('#scene-view').append(cell);
+            var cell_mask = document.createElement('div');
+            cell_mask.innerHTML='<div class="d">'+
+                '<i class="fa fa-dashboard"></i>'+
+                cell_info.download_times+
+                '</div>';
+            cell.appendChild(cell_mask);
+        });
+        //console.log(json.scene[0]);
+    }
+
+    function fill_scene_thumb(id, thumb){
+
+    }
+
+    load_scene(0, []);
+})();
