8a5f4acdd22f34ea342bbd2b2e5abf51d407a582
ase_chujy <1454212589@qq.com>
2014-12-27 21:14:31 +0800

    标签管理界面

---
+++ code/server/www/admin/c-tag.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>美家秀秀·标签管理</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- DataTable CSS-->
+    <link href="css/plugins/dataTables/jquery.dataTables.css">
+    <link href="css/plugins/dataTables/dataTables.bootstrap.css">
+
+    <!-- Custom CSS -->
+    <link href="css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="css/font-awesome.css" rel="stylesheet" type="text/css">
+
+    <!-- Meijia Customize css-->
+    <link href="css/meijia-core.css" rel="stylesheet">
+    <link href="css/meijia-scene.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <!--<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="mj-titlebar navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class=" navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a id="mj-main-title" class="navbar-brand" href="index.php">美家秀秀·标签管理</a>
+            </div>
+            <!-- /.navbar-header -->
+
+            <ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown">
+                    <a id="mj-dropdown-toggle" class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="mj-sidebar-item dropdown-menu dropdown-user">
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i><span>账户信息</span></a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i><span>设置</span></a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="login.php"><i class="fa fa-sign-out fa-fw"></i><span>退出登录</span></a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="mj-search-placeholder form-control" placeholder="搜索一下...">
+                                <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a class="mj-sidebar-item" href="index.php"><i class="fa fa-dashboard fa-fw"></i><span>控制台</span></a>
+                        </li>
+                        <li>
+                            <a class="mj-sidebar-item active" href="scene.php"><i class="fa fa-th fa-fw"></i><span>场景管理</span></a>
+                        </li>
+                        <li>
+                            <a class="mj-sidebar-item active" href="c-tag.php"><i class="fa fa-th fa-fw"></i><span>标签管理</span></a>
+                        </li>
+
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+        <div id="page-wrapper">
+            <div id="page-mask"></div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 id="page-title" class="mj-page-title page-header">浏览标签数据字典</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <div class="row">
+                <?php
+                if(isset($_POST['mode'])){
+                    if($_POST['mode'] == 'modify-success'){
+                        echo '<div class="alert alert-success tobefade" role="alert"><i class="fa fa-check"></i>&nbsp;成功修改标签数据</div>';
+                    }
+                    if($_POST['mode'] == 'add-success'){
+                        echo '<div class="alert alert-success tobefade" role="alert"><i class="fa fa-check"></i>&nbsp;成功增加标签</div>';
+                    }
+                    if($_POST['mode'] == 'delete-success'){
+                        echo '<div class="alert alert-success tobefade" role="alert"><i class="fa fa-check"></i>&nbsp;成功删除标签</div>';
+                    }
+                }
+                ?>
+
+            </div>
+            <div class="row">
+
+                <div class="col-lg-12">
+                    <button id="bt-new-tag" type="button" class="btn btn-success" style="float:right"><i class="fa fa-plus"></i>&nbsp;添加标签</button>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <!--<div class="row" id="scene-table">
+
+            </div>-->
+            <table id="tag-table" class="table table-striped table-bordered dataTable no-footer display compact" cellspacing="0" width="100%">
+                <thead>
+                <tr>
+                    <th>标签ID</th>
+                    <th>标签名称</th>
+                    <th>标签描述</th>
+                </tr>
+                </thead>
+
+                <tfoot>
+                <tr>
+                    <th>标签ID</th>
+                    <th>标签名称</th>
+                    <th>标签描述</th>
+                </tr>
+                </tfoot>
+            </table>
+            <!-- /.row -->
+
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="js/plugins/metisMenu/metisMenu.min.js"></script>
+
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/sb-admin-2.js"></script>
+
+    <!-- DataTable plugin JS-->
+    <script src="js/plugins/dataTables/jquery.dataTables.js"></script>
+    <script src="js/plugins/dataTables/dataTables.bootstrap.js"></script>
+
+    <!-- Meijia JS-->
+    <script src="js/config.js"></script>
+
+    <script src="js/c-tags.js"></script>
+
+</body>
+
+</html>
